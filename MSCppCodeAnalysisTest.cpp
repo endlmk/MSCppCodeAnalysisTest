40cdc1a24a8be4f5c1ee481b02b2cfc50cfc7bfb
endlmk <1269467+endlmk@users.noreply.github.com>
2020-03-06 07:22:32 +0900

    Initial Commit.

---
+++ MSCppCodeAnalysisTest.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+// MS Code Analysis
+int main()
+{
+	int a[10];
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		if (i == 2)
+		{
+			a[i]; // C6001 not Detected.
+		}
+		if (i == 1)
+		{
+			a[i]; // C6001 Detected.
+		}
+		//a[i]; // C6001 Detected.
+	}
+}
